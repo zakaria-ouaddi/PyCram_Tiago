79bb2629cac45cb0927b6c51f60f621cc9d4a650
Zakaria Ouaddi <zakariaouaddi2@gmail.com>
2024-10-29 10:50:44 +0100

    opens a drawer and looks for a cup

---
+++ tiago_robot_work/look_for_cup.py
@@ -0,0 +1,102 @@
+from pycram.worlds.bullet_world import BulletWorld
+from pycram.world_concepts.world_object import Object
+from pycram.process_module import simulated_robot, with_simulated_robot
+from pycram.designators.location_designator import *
+from pycram.designators.action_designator import *
+from pycram.designators.object_designator import *
+from pycram.datastructures.enums import Grasp, WorldMode
+from pycram.designators.motion_designator import *
+
+
+world=BulletWorld(WorldMode.GUI)
+
+apartment=Object('apartment',ObjectType.ENVIRONMENT,'apartment.urdf')
+pr2=Object('pr2',ObjectType.ROBOT,'pr2.urdf',pose=Pose([1.2,2,0]))
+
+milk=Object('milk',ObjectType.MILK,'milk.stl',pose=Pose([2.4, 2.2,1]))
+
+#objects inside drawers
+bowl=Object('bowl',ObjectType.BOWL,'bowl.stl',pose=Pose([2.4, 2.2, 0.68]))
+spoon=Object('spoon',ObjectType.SPOON,'spoon.stl',pose=Pose([2.5, 2.3, 0.86]))
+
+#adding objects to drawrs
+apartment.attach(spoon, 'cabinet10_drawer_top')
+apartment.attach(bowl, 'cabinet10_drawer_middle')
+
+pick_pose = Pose([2.4, 2.15, 1])
+
+robot_desig = BelieveObject(names=["pr2"])
+apartment_desig = BelieveObject(names=["apartment"])
+
+
+@with_simulated_robot
+def move_and_detect(obj_type):
+    NavigateAction(target_locations=[Pose([1.7, 2, 0])]).resolve().perform()
+
+    LookAtAction(targets=[pick_pose]).resolve().perform()
+
+    object_desig = DetectAction(BelieveObject(types=[obj_type])).resolve().perform()
+
+    return object_desig
+
+
+@with_simulated_robot
+def transport_obj(obj,obj_typ,handle_name,target_pose):
+
+    handle_desig = ObjectPart(names=[handle_name], part_of=apartment_desig.resolve())
+    drawer_open_loc = AccessingLocation(handle_desig=handle_desig.resolve(),
+                                        robot_desig=robot_desig.resolve()).resolve()
+
+    NavigateAction([drawer_open_loc.pose]).resolve().perform()
+
+
+    OpenAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
+    obj.detach(apartment)
+
+    LookAtAction([apartment.get_link_pose(handle_name)]).resolve().perform()
+
+    obj_desig = DetectAction(BelieveObject(types=[obj_typ])).resolve().perform()
+
+    pickup_arm = Arms.LEFT if drawer_open_loc.arms[0] == Arms.RIGHT else Arms.RIGHT
+    PickUpAction(obj_desig, [pickup_arm], [Grasp.TOP]).resolve().perform()
+
+    ParkArmsAction([Arms.LEFT if pickup_arm == Arms.LEFT else Arms.RIGHT]).resolve().perform()
+
+    CloseAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
+
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+    placing_loc = CostmapLocation(target=target_pose, reachable_for=robot_desig.resolve()).resolve()
+
+    NavigateAction([placing_loc.pose]).resolve().perform()
+
+    PlaceAction(obj_desig, [target_pose], [pickup_arm]).resolve().perform()
+
+
+#handle_cab10_t
+#handle_cab11_t
+#handle_cab5_t
+
+spoon_pose=Pose([4.85, 3.3, 0.8], [0, 0, 1, 1])
+bowl_pose=Pose([2.4, 2.2,0.99])
+tst_pose=Pose([2.4, 2.2,0.99])
+'''for nm in apartment.link_name_to_id:
+    print(nm)'''
+
+with simulated_robot:
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+    MoveTorsoAction([0.4]).resolve().perform()
+
+    #detect milk and transport it
+    milk_desig=move_and_detect(ObjectType.MILK)
+    TransportAction(milk_desig,[Arms.RIGHT],[Pose([4.8, 3.55, 0.8])]).resolve().perform()
+
+
+    transport_obj(spoon,ObjectType.SPOON,"handle_cab10_t",spoon_pose)
+    transport_obj(bowl,ObjectType.BOWL,"handle_cab10_m",bowl_pose)
+    #transport_obj(bowl,ObjectType.BOWL,"handle_cab1_drawer_bottom",tst_pose)
+
+    MoveTorsoAction([0.2]).resolve().perform()
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+
